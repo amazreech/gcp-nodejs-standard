b2cc683b738920da48d03ee461a42e092ce24bb7
amazreech <136762730+amazreech@users.noreply.github.com>
2024-03-21 14:19:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
 });
 
+const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`helloworld: listening on port ${port}`);
 });
